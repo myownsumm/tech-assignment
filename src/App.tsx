82c588f202c0483da196f363715abee8631b2b2e
myownsumm <murmur_spree_0m@icloud.com>
2026-01-13 23:27:00 +0100

    Install MUI, configure left side panel.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
+import { Box, Paper } from '@mui/material';
 import { Map } from './components/Map';
 import './App.css';
 
 function App() {
   return (
-    <div className="app-container">
-      <Map />
-    </div>
+    <Box sx={{ display: 'flex', height: '100vh', width: '100vw' }}>
+      <Paper
+        elevation={2}
+        sx={{
+          width: 320,
+          minWidth: 320,
+          height: '100%',
+          borderRadius: 0,
+          display: 'flex',
+          flexDirection: 'column',
+          zIndex: 1,
+        }}
+      >
+        {/* Panel content will go here */}
+      </Paper>
+      <Box sx={{ flex: 1, position: 'relative', overflow: 'hidden' }}>
+        <Map />
+      </Box>
+    </Box>
   );
 }
 
